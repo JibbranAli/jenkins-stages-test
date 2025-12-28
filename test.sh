b84e8415eb4881a10885c2b8515a3ff43aefb1e2
Syed Jibbran Ali <90756497+JibbranAli@users.noreply.github.com>
2025-12-28 22:45:19 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-echo "Testing website..."
-curl -f http://localhost || exit 1
 
+echo "Testing website on DEV..."
+
+curl -f http://localhost:8081 || exit 1
+
+echo "DEV website is reachable"
